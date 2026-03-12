83567c08af3d3905139d45af1a304b039577da5d
Admin <Admin@DESKTOP-K078S8T>
2026-03-12 22:01:41 +0800

    Первая версия приложения

--- pechat/Form1.Designer.cs
+++ pechat/Form1.Designer.cs
@@ -2,15 +2,16 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTargetText;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblErrors;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Timer timer1;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,42 +21,108 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTargetText = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblErrors = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // label1
+            // lblTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Хоботова Яна Павловна";
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(26, 43);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(226, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Тренировка печатания";
+            // 
+            // lblTargetText
+            // 
+            this.lblTargetText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTargetText.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lblTargetText.Location = new System.Drawing.Point(31, 100);
+            this.lblTargetText.Name = "lblTargetText";
+            this.lblTargetText.Size = new System.Drawing.Size(420, 50);
+            this.lblTargetText.TabIndex = 2;
+            this.lblTargetText.Text = "текст";
+            this.lblTargetText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(31, 175);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(420, 20);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(31, 220);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(160, 35);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Начать заново";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(31, 280);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(73, 13);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "Время: 0 сек";
+            // 
+            // lblErrors
+            // 
+            this.lblErrors.AutoSize = true;
+            this.lblErrors.Location = new System.Drawing.Point(31, 310);
+            this.lblErrors.Name = "lblErrors";
+            this.lblErrors.Size = new System.Drawing.Size(59, 13);
+            this.lblErrors.TabIndex = 6;
+            this.lblErrors.Text = "Ошибки: 0";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(31, 340);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(97, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Статус: ожидание";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(490, 390);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblErrors);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblTargetText);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тренажёр скоропечатания";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
-
--- pechat/Form1.cs
+++ pechat/Form1.cs
@@ -1,20 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace pechat
 {
     public partial class Form1 : Form
     {
+        private string targetText = "мама мыла раму";
+        private Stopwatch stopwatch = new Stopwatch();
+        private bool started = false;
+
         public Form1()
         {
             InitializeComponent();
+            lblTargetText.Text = targetText;
+            lblTime.Text = "Время: 0 сек";
+            lblErrors.Text = "Ошибки: 0";
+            lblResult.Text = "Статус: ожидание";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            started = true;
+
+            lblTime.Text = "Время: 0 сек";
+            lblErrors.Text = "Ошибки: 0";
+            lblResult.Text = "Статус: тренировка началась";
+
+            timer1.Start();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            if (!started)
+                return;
+
+            string userText = txtInput.Text;
+            int errors = CountErrors(userText, targetText);
+
+            lblErrors.Text = $"Ошибки: {errors}";
+
+            if (userText == targetText)
+            {
+                stopwatch.Stop();
+                timer1.Stop();
+                started = false;
+
+                lblResult.Text = $"Готово! Время: {stopwatch.Elapsed.Seconds} сек, ошибок: {errors}";
+                MessageBox.Show(
+                    $"Текст введён верно!\nВремя: {stopwatch.Elapsed.Seconds} сек\nОшибок: {errors}",
+                    "Результат",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private int CountErrors(string userText, string correctText)
+        {
+            int errors = 0;
+            int minLength = Math.Min(userText.Length, correctText.Length);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (userText[i] != correctText[i])
+                    errors++;
+            }
+
+            if (userText.Length > correctText.Length)
+                errors += userText.Length - correctText.Length;
+
+            return errors;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (started)
+            {
+                lblTime.Text = $"Время: {stopwatch.Elapsed.Seconds} сек";
+            }
         }
     }
 }
